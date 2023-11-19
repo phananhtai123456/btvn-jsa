80f90bcbf7a6aeab251afb32059ef52011741880
Your Name <you@example.com>
2023-11-19 10:10:52 +0700

    tai

---
+++ btvn tuần 2/index.js
@@ -0,0 +1,30 @@
+//Bài tập 1
+var fruit=[
+    {
+        type:'Grapes',
+        quantity:15
+    },
+    {
+        type:'Bananna',
+        quantity:10
+    },
+    {
+        type:'orange',
+        quantity:5
+    }
+]
+
+//Bài tập 2
+for (var i = 0; i < 10; i++) {
+    var typefruit = fruit(i).type
+    var quantityfruit = fruit(i).quantity
+    console.log('i have 15 grapes, 10 banana and 5 orange')
+}
+
+//Bài tập 3
+var Husky = {
+    name: 'Alaska',
+    legs: 4,
+    color: 'brown'
+}
+console.log(Husky)
